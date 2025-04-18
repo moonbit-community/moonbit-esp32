9e571d40ce00ced877d8c3039e767eb55ecf63cf
Li Junchen <lijunchen95@outlook.com>
2025-04-18 11:28:57 +0800

    format

--- src/components/gpio/moonbit_esp32_gpio_stub.c
+++ src/components/gpio/moonbit_esp32_gpio_stub.c
@@ -17,24 +17,19 @@
 #include "moonbit.h"
 #include <driver/gpio.h>
 
-esp_err_t __wrap_gpio_config(
-    uint64_t pin_bit_mask,
-    int32_t mode,
-    int32_t pull_up_en,
-    int32_t pull_down_en,
-    int32_t intr_type)
-{
-    gpio_config_t io_conf = {
-        .pin_bit_mask = pin_bit_mask,
-        .mode = mode,
-        .pull_up_en = pull_up_en,
-        .pull_down_en = pull_down_en,
-        .intr_type = intr_type,
-    };
-    return gpio_config(&io_conf);
+esp_err_t __wrap_gpio_config(uint64_t pin_bit_mask, int32_t mode,
+                             int32_t pull_up_en, int32_t pull_down_en,
+                             int32_t intr_type) {
+  gpio_config_t io_conf = {
+      .pin_bit_mask = pin_bit_mask,
+      .mode = mode,
+      .pull_up_en = pull_up_en,
+      .pull_down_en = pull_down_en,
+      .intr_type = intr_type,
+  };
+  return gpio_config(&io_conf);
 }
 
-esp_err_t __wrap_gpio_set_level(int gpio_num, uint32_t level)
-{
-    return gpio_set_level(gpio_num, level);
+esp_err_t __wrap_gpio_set_level(int gpio_num, uint32_t level) {
+  return gpio_set_level(gpio_num, level);
 }
--- src/components/task/moonbit_esp32_task_stub.c
+++ src/components/task/moonbit_esp32_task_stub.c
@@ -14,26 +14,19 @@
  * // limitations under the License.
  */
 
-#include <stdint.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
+#include <stdint.h>
 
-int32_t __wrap_xTaskCreate(
-    void *pxTaskCode,
-    uint8_t *pcName,
-    uint32_t usStackDepth,
-    uint32_t uxPriority)
-{
-    int32_t ret = xTaskCreate(pxTaskCode, (char *)pcName, usStackDepth, NULL, uxPriority, NULL);
-    return ret;
+int32_t __wrap_xTaskCreate(void *pxTaskCode, uint8_t *pcName,
+                           uint32_t usStackDepth, uint32_t uxPriority) {
+  int32_t ret = xTaskCreate(pxTaskCode, (char *)pcName, usStackDepth, NULL,
+                            uxPriority, NULL);
+  return ret;
 }
 
-void __wrap_vTaskDelay(int32_t xTicksToDelay)
-{
-    vTaskDelay(xTicksToDelay);
-}
+void __wrap_vTaskDelay(int32_t xTicksToDelay) { vTaskDelay(xTicksToDelay); }
 
-int32_t __wrap_pdMS_TO_TICKS(int32_t xTimeInMs)
-{
-    return pdMS_TO_TICKS(xTimeInMs);
+int32_t __wrap_pdMS_TO_TICKS(int32_t xTimeInMs) {
+  return pdMS_TO_TICKS(xTimeInMs);
 }
