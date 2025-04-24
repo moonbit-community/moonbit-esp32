7c549f783788c75eb7e26d2cc4f63714e860e46b
Li Junchen <lijunchen95@outlook.com>
2025-04-24 16:16:02 +0800

    bump version to 0.1.7 in moon.mod.json and include esp_lcd_panel_st7789.h in moonbit_esp32_lcd_stub.c

--- src/components/lcd/moonbit_esp32_lcd_stub.c
+++ src/components/lcd/moonbit_esp32_lcd_stub.c
@@ -17,6 +17,7 @@
 #include "esp_err.h"
 #include "esp_lcd_panel_io.h"
 #include "esp_lcd_panel_ops.h"
+#include "esp_lcd_panel_st7789.h"
 #include "esp_lcd_panel_vendor.h"
 #include "esp_lcd_types.h"
 #include <esp_lcd_io_spi.h>
